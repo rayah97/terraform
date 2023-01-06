786ee7b823f32ae7ec466c5b3ec8b25a55caeb80
rayah97 <harutyunyan.raya97@gmail.com>
2023-01-07 00:05:38 +0400

    added run action

---
+++ main.tf
@@ -0,0 +1,8 @@
+# provider "aws" {
+#     region = "us-east-1"
+# }
+# resource "aws_instance" "myec2"{ 
+
+#    ami = "ami-0b5eea76982371e91" 
+#    instance_type = "t2.micro"
+# }
---
+++ tf2.tf
@@ -0,0 +1,9 @@
+# provider "aws" {
+#   access_key = "${var.aws_access_key}"
+#   secret_key = "${var.aws_secret_key}"
+#   region     = "us-east-1"
+# }
+resource "aws_s3_bucket" "example" {
+  bucket = "example-bucketjjjjjjjjjjjjjj"
+  
+}
