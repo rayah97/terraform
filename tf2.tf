704f60586043921bd6d8d7c70c63dc3469151579
rayah97 <harutyunyan.raya97@gmail.com>
2023-01-07 00:10:58 +0400

    added run action11

--- tf2.tf
+++ tf2.tf
@@ -3,6 +3,9 @@
 #   secret_key = "${var.aws_secret_key}"
 #   region     = "us-east-1"
 # }
+provider "aws" {
+  region = "us-east-1"
+}
 resource "aws_s3_bucket" "example" {
   bucket = "example-bucketjjjjjjjjjjjjjj"
   
